f085c26b5ea1808df90d730033c96071cfb5499c
thtngan <thtngan>
2021-08-16 19:20:41 +0700

    fix menu bar searchPage

--- js/scriptSearchPg.js
+++ js/scriptSearchPg.js
@@ -94,6 +94,7 @@ inputBox.onkeyup = (e)=>{
         /*Create array to suggestion*/
         emptyArray = suggestions.filter((data)=>{
             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
         });
         emptyArray = emptyArray.map((data)=>{
