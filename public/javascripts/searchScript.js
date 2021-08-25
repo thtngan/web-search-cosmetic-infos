3bf0e40f7192e0eaf5882a39063859f6d0c3f92e
thtngan <thtngan>
2021-08-25 20:06:48 +0700

    selectable Search product

--- public/javascripts/searchScript.js
+++ public/javascripts/searchScript.js
@@ -4,39 +4,49 @@ $('#myDiv').on('widthChanged', function () {
   // console.log($('.horizontal-scroll-wrapper').height());
 });
 
+
+
 //Search script
 $('#searchbar').autocomplete({
-  source: function(req, res){
+  source: function (req, res) {
     $.ajax({
-      url:"/search/autocompleteSearch",
+      url: "/search/autocompleteSearch",
       dataType: "json",
       type: "GET",
-      data: req, 
-      success: function(data){
+      data: req,
+      success: function (data) {
         console.log(data);
         res(data);
+        if (data[0].label == "Không tìm thấy sản phẩm")
+          document.getElementById('numberResult').innerHTML = 0 + " kết quả";
+        else
+          document.getElementById('numberResult').innerHTML = data.length + " kết quả";
+
       },
-      err: function(err){
+      err: function (err) {
         console.log(err.status);
       }
     });
   },
-  
-}).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-  if (item.picture == null){
-    var inner_html = '<h4><b>' + item.label + '</b></h4>>';
+
+}).data("ui-autocomplete")._renderItem = function (ul, item) {
+  if (item.picture == null) {
+    var inner_html = '<h4><b>' + item.label + '</b></h4>';
+
   }
-  else{
-    var inner_html = '<div class="list_item_container"><div class="imageSearch"><img src="' + item.picture + '" ></div><div class="labelSearch"><h4><b>' + item.label + '</b></h4></div></div>';
+  else {
+    var inner_html = '<div class="list_item_container"><a href="/info/' + item.label + '"><div class="imageSearch"><img src="' + item.picture + '" ></div><div class="labelSearch"><h4><b>' + item.label + '</b></h4></div></a></div>';
   }
-    return $( "<li></li>" )
-          .data( "item.autocomplete", item )
-          .append(inner_html)
-          .appendTo( ul );  
+  return $("<li></li>")
+    .data("item.autocomplete", item)
+    .append(inner_html)
+    .appendTo(ul);
 };
 
-$('#ui-id-1').bind('DOMSubtreeModified', function() {
-  $('#containerSearch').html($('#ui-id-1').html());
+$('#ui-id-1').bind('DOMSubtreeModified', function () {
+  $('#containerItem').html($('#ui-id-1').html());
 });
 
+
 //Filter script
+
