47550ccc37c54c8abcb171b51e13d9c83cd344c7
thtngan <thtngan>
2021-08-26 00:02:51 +0700

    filter Search

--- routes/search.js
+++ routes/search.js
@@ -4,40 +4,42 @@ var mongoose = require('mongoose')
 var product = require('../models/model')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('search');
+router.get('/', function (req, res, next) {
+    res.render('search');
 });
 
-router.get('/autocompleteSearch/', function(req, res, next) {
-  var regex = new RegExp(req.query["term"], 'i');
-  
-  var productFilter = product.find({Name:regex});
-  productFilter.exec(function(err, data){
-      //console.log(data);
-      var result =[];
-      if (!err){
-          if (data && data.length && data.length >0){
-             data.forEach(prod =>{
-                 //console.log(prod);
-                  let obj = {
-                      id: prod._id,
-                      label: prod.Name,
-                      picture: prod.Photos[0].Url1
-                  };
-                  result.push(obj);
-             });
-          }
-          else{
-              let obj = {
-                  label: 'Không tìm thấy sản phẩm',
-                  picture: null
-              }
-              result.push(obj);
-          }
-          //console.log(result);
-          res.jsonp(result);
-      }
-  });
+router.get('/autocompleteSearch/', function (req, res, next) {
+    var regex = new RegExp(req.query["term"], 'i');
+
+    var productFilter = product.find({ Name: regex });
+    productFilter.exec(function (err, data) {
+        //console.log(data);
+        var result = [];
+        if (!err) {
+            if (data && data.length && data.length > 0) {
+                data.forEach(prod => {
+                    //console.log(prod);
+                    let obj = {
+                        id: prod._id,
+                        label: prod.Name,
+                        picture: prod.Photos[0].Url1,
+                        skin: prod.Skin
+                    };
+                    result.push(obj);
+                });
+            }
+            else {
+                let obj = {
+                    label: 'Không tìm thấy sản phẩm',
+                    picture: null,
+                    skin: null
+                }
+                result.push(obj);
+            }
+            //console.log(result);
+            res.jsonp(result);
+        }
+    });
 });
 
 module.exports = router;
