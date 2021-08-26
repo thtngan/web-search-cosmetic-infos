4766da6c6822a0295518a23fe34285e5c86261ff
thtngan <thtngan>
2021-08-26 23:39:24 +0700

    Edit admin/user

--- routes/admin.js
+++ routes/admin.js
@@ -2,18 +2,24 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose')
 var product = require('../models/model')
+var user = require('../models/user.model')
 
 /* GET admin page */
 router.get('/', (req, res) => {
     res.render('../views/admin/admin')
 });
 
-router.get('/products', (req, res) =>{
+router.get('/products', (req, res) => {
     res.render('../views/admin/adProduct')
 })
 
-router.get('/users',(req, res) =>{
-    res.render('../views/admin/adUser')
-})
+router.get('/users', function (req, res, next) {
+    var productFilter = product.find({});
+
+    user.find({ View: { $eq: 0 } }, (err, products) => {
+        console.log(products);
+        res.render('../views/admin/adUser', { name: products });
+    })
+});
 
 module.exports = router;
